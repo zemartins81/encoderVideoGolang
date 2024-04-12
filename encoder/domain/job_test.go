97b7dc8fe5f98c69b59fd7193d52e64ef1fa52e3
José C. Martins <zemartins81@gmail.com>
2024-04-11 23:22:38 -0300

    feat: test job

--- encoder/domain/job_test.go
+++ encoder/domain/job_test.go
@@ -1,1 +1,20 @@
 package domain_test
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/zemartins81/encoderVideoGolang/domain"
+	"testing"
+	"time"
+)
+
+func TestNewJob(t *testing.T) {
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	job, err := domain.NewJob("path", "converted", video)
+	require.NotNil(t, job)
+	require.Nil(t, err)
+}
